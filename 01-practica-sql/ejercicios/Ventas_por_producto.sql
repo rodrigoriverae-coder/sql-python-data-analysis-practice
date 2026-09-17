74ab89de765e70cada61a7bbac5a5aa101756576
rodrigoriverae-coder <rodrigoriverae@gmail.com>
2026-09-17 14:29:33 -0500

    Update Ventas_por_producto.sql

--- 01-practica-sql/ejercicios/Ventas_por_producto.sql
+++ 01-practica-sql/ejercicios/Ventas_por_producto.sql
@@ -1,13 +1,15 @@
 WITH reporte_base AS (
-select
-	p.id_producto,
-	p.nombre_prod,
-	coalesce (sum(v.cantidad),0) as total_unidades_vendidas,
-	round(coalesce (sum(P.precio*V.cantidad),0),2) as ingreso_total
-from productos as P
-left join ventas as V
-on P.id_producto=V.id_producto
-group by p.id_producto, p.nombre_prod
-order by p.id_producto
+    SELECT
+        p.id_producto,
+        p.nombre_prod,
+        COALESCE(SUM(v.cantidad), 0) AS total_unidades_vendidas,
+        ROUND(COALESCE(SUM(p.precio * v.cantidad), 0), 2) AS ingreso_total
+    FROM productos AS p
+    LEFT JOIN ventas AS v
+        ON p.id_producto = v.id_producto
+    GROUP BY p.id_producto, p.nombre_prod
 )
-SELECT * FROM reporte_base WHERE ingreso_total = 0;
+SELECT *
+FROM reporte_base
+WHERE ingreso_total = 0
+ORDER BY id_producto;
